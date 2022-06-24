13d4dae0424f0af634b485bf6726f6804ad1fe2c
yunosuke-github <yunosuke.github@gmail.com>
2022-06-25 00:33:18 +0900

    Swagger導入

--- kajinote_api/settings.py
+++ kajinote_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
+    'rest_framework_swagger',
     'rest_framework',
     'api',
 ]
