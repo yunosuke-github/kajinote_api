3c82b7a4c15b4e6fb66566efb62e6bd323c0fb92
yunosuke-github <yunosuke.github@gmail.com>
2022-06-25 11:17:14 +0900

    approve_flgを更新バリデーション追加

--- api/serializers/housework_history_serializer.py
+++ api/serializers/housework_history_serializer.py
@@ -81,6 +81,8 @@ class HouseworkHistorySerializer(serializers.ModelSerializer):
             housework_history.housework_id = housework.id
             housework_history.point = housework.point
         if 'approve_flg' in validate_data.keys():
+            if validate_data['approve_flg'] not in [0, 1]:
+                raise ValidationError('Specify 0 or 1 for approve_flg.')
             housework_history.approve_flg = validate_data['approve_flg']
         housework_history.save()
         return HouseworkHistorySerializer(housework_history).data
